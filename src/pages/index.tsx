0b173e60ed3b4bda73ae2a97f8be1b55204dbbba
Raymond Tetteh <raymondtetteh111@gmail.com>
2023-02-10 22:44:07 +0000

    fix: minor changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,18 +9,6 @@ import ProjectCard from '@/components/projects/ProjectCard';
 import Seo from '@/components/Seo';
 import TechnologyCard from '@/components/technologies/TechnologyCard';
 
-/**
- * SVGR Support
- * Caveat: No React Props Type.
- *
- * You can override the next-env if the type is important to you
- * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
- */
-
-// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
-// Before you begin editing, follow all comments with `STARTERCONF`,
-// to customize the default configuration.
-
 const HomePage = () => {
   gsap.registerPlugin(ScrollTrigger);
 
@@ -58,21 +46,6 @@ const HomePage = () => {
         scrub: 0.2,
       },
     });
-
-    // tl.current = gsap.to(
-    //   q("#stacks-tab"),
-    //   {
-    //     scrollTrigger: {
-    //       trigger: q("#stacks"),
-    //       start: "top 80%",
-    //       end: "bottom 80%",
-    //       onEnter: () => setActiveTab('stacks'),
-    //       onEnterBack: () => setActiveTab('stacks'),
-    //       scroller: "#section-2",
-    //       scrub: 0.2
-    //     }
-    //   }
-    // );
   }, [q]);
 
   return (
@@ -93,7 +66,7 @@ const HomePage = () => {
 
           <div className='z-1 absolute hidden min-h-screen min-w-full bg-[#151515]/[0.8] md:block'></div>
 
-          <div className='z-10 flex flex-col overflow-hidden md:mx-60 md:h-screen md:flex-row md:gap-20'>
+          <div className='z-10 flex flex-col overflow-hidden md:mx-20 md:h-screen md:flex-row md:gap-20 2xl:mx-60'>
             {/* Left  */}
             <div className='relative flex h-screen flex-col gap-10 md:w-2/5'>
               <video
@@ -112,20 +85,17 @@ const HomePage = () => {
                   <h1 className='whitespace-nowrap text-6xl md:text-7xl'>
                     I&apos;m Raymond.
                   </h1>
-                  <h3 className='mt-3'>A passionate Software Engineer</h3>
+                  <h3 className='mt-3'>A Passionate Software Engineer.</h3>
                 </div>
 
                 <div>
                   <p>
-                    I am Raymond Tetteh. A 22-year-old software engineer based
-                    in Ghana. I am a{' '}
-                    <span className='font-bold underline'>
-                      Computer Engineering
-                    </span>{' '}
-                    student but I have been a{' '}
+                    I am Raymond Tetteh. A passionate software engineer based in
+                    Ghana. I am a{' '}
                     <span className='font-bold underline'>self-taught</span>{' '}
-                    developer for over 3 years. I enjoy taking mobile apps and
-                    beautiful web applications from design to a{' '}
+                    developer for andn have been building stuff for over 3
+                    years. I enjoy taking mobile apps and beautiful web
+                    applications from design to a{' '}
                     <span className='font-bold underline'>
                       fully working product.
                     </span>{' '}
@@ -274,7 +244,7 @@ const HomePage = () => {
             <div
               id='section-2'
               ref={ref}
-              className='no-scrollbar z-10 flex flex-col gap-4 overflow-x-auto overflow-y-scroll px-4 pb-4 md:w-3/5 md:gap-40 md:px-10 md:pb-40'
+              className='no-scrollbar z-10 flex flex-col gap-4 overflow-x-auto overflow-y-scroll px-4 py-40 pb-4 md:w-3/5 md:px-14 md:pb-40'
             >
               <div id='projects' className='relative pt-4 md:pt-14'>
                 <div className='sticky top-0 left-0 right-0 py-5 uppercase tracking-widest shadow-md md:hidden'>
@@ -319,12 +289,12 @@ const HomePage = () => {
                 </div>
               </div>
 
-              <div id='technologies' className='relative'>
+              <div id='technologies' className='relative mt-40'>
                 <div className='sticky top-0 left-0 right-0 py-5 uppercase tracking-widest shadow-md md:hidden'>
                   Technologies
                 </div>
                 <div className='flex-col gap-4 md:flex md:flex-row'>
-                  <div className='mb-4 flex flex-col gap-4 md:w-1/2'>
+                  <div className='mb-4 flex flex-col gap-4 px-2 md:w-1/2'>
                     <TechnologyCard
                       icon={
                         <svg
@@ -374,8 +344,37 @@ const HomePage = () => {
                       desc='I have used UI/UX tools such as Figma and Adobe XD.'
                       rating='3.5'
                     />
+                    {/* <TechnologyCard
+                      icon={
+                        <svg
+                          className='mr-3 h-10 w-10'
+                          fill='#ffffff'
+                          xmlns='http://www.w3.org/2000/svg'
+                          viewBox='0 0 30 30'
+                          width='90px'
+                          height='90px'
+                        >
+                          <path
+                            d='M93937 72393c0 4102 443 7428 1219 9867 887 2439 1996 5100 3548 7982 554 887 776 1774 776 2550 0 1109-665 2217-2106 3326l-6985 4656c-998 665-1995 998-2882 998-1109 0-2217-554-3326-1552-1552-1663-2882-3437-3991-5211-1109-1885-2217-3991-3437-6541-8648 10200-19512 15299-32594 15299-9312 0-16740-2661-22172-7982-5432-5322-8204-12417-8204-21286 0-9424 3326-17073 10089-22838s15743-8647 27161-8647c3769 0 7650 332 11752 887 4102 554 8315 1441 12749 2439v-8093c0-8426-1774-14301-5211-17738-3548-3437-9534-5100-18071-5100-3880 0-7871 443-11973 1441s-8093 2217-11973 3769c-1774 776-3104 1219-3880 1441s-1330 332-1774 332c-1552 0-2328-1109-2328-3437v-5432c0-1774 222-3104 776-3880s1552-1552 3104-2328c3880-1996 8537-3659 13969-4989C43606 885 49370 220 55468 220c13193 0 22838 2993 29046 8980 6098 5987 9202 15077 9202 27272v35920h222zM48926 89244c3659 0 7428-665 11419-1995s7539-3769 10532-7095c1774-2106 3104-4435 3770-7095 665-2661 1108-5876 1108-9645v-4656c-3215-776-6652-1441-10199-1885-3548-443-6984-665-10421-665-7428 0-12860 1441-16519 4435-3659 2993-5432 7206-5432 12749 0 5211 1330 9091 4102 11751 2661 2772 6541 4102 11641 4102zm89023 11973c-1996 0-3326-332-4213-1109-887-665-1663-2217-2328-4324l-26053-85697c-665-2217-998-3658-998-4434 0-1774 887-2772 2661-2772h10865c2106 0 3548 333 4324 1109 887 665 1552 2217 2217 4324l18625 73391 17295-73391c554-2217 1219-3659 2106-4324s2439-1109 4435-1109h8869c2106 0 3548 333 4435 1109 887 665 1663 2217 2106 4324l17516 74278 19180-74278c665-2217 1441-3659 2217-4324 887-665 2328-1109 4324-1109h10310c1774 0 2772 887 2772 2772 0 554-111 1109-222 1774s-333 1552-776 2772l-26718 85697c-665 2217-1441 3658-2328 4324-887 665-2328 1109-4213 1109h-9534c-2107 0-3548-333-4435-1109s-1663-2217-2106-4435l-17184-71507-17073 71396c-554 2217-1220 3658-2107 4434s-2439 1109-4434 1109h-9534zm142459 2993c-5765 0-11530-665-17073-1995s-9867-2772-12749-4435c-1774-998-2993-2106-3437-3104-443-998-665-2106-665-3104v-5654c0-2328 887-3437 2550-3437 665 0 1330 111 1995 333s1663 665 2772 1109c3769 1663 7871 2993 12195 3880 4435 887 8758 1330 13193 1330 6984 0 12417-1220 16186-3659s5765-5987 5765-10532c0-3104-998-5654-2993-7760-1996-2107-5765-3991-11197-5765l-16075-4989c-8093-2550-14080-6319-17738-11308-3658-4878-5543-10310-5543-16075 0-4656 998-8758 2993-12306s4656-6652 7982-9091c3326-2550 7095-4434 11530-5765S279190-2 284068-2c2439 0 4989 111 7428 443 2550 333 4878 776 7206 1219 2217 554 4324 1109 6319 1774s3548 1330 4656 1996c1552 887 2661 1774 3326 2771 665 887 998 2107 998 3659v5211c0 2328-887 3548-2550 3548-887 0-2328-444-4213-1331-6319-2882-13415-4324-21286-4324-6319 0-11308 998-14745 3104s-5211 5321-5211 9867c0 3104 1109 5765 3326 7871s6319 4213 12195 6097l15743 4989c7982 2550 13747 6098 17184 10643s5100 9756 5100 15521c0 4767-998 9091-2882 12860-1996 3770-4656 7095-8093 9756-3437 2771-7539 4767-12306 6208-4989 1552-10199 2328-15854 2328z'
+                            fill='#252f3e'
+                          />
+                          <path
+                            className='fil1'
+                            d='M301362 158091c-36474 26940-89467 41241-135031 41241-63858 0-121395-23614-164854-62859-3437-3104-332-7317 3770-4878 47006 27272 104988 43791 164964 43791 40465 0 84921-8426 125830-25721 6097-2772 11308 3991 5321 8426z'
+                          />
+                          <path
+                            className='fil1'
+                            d='M316550 140796c-4656-5987-30820-2883-42682-1441-3548 443-4102-2661-887-4989 20842-14634 55099-10421 59090-5543 3991 4989-1109 39246-20620 55653-2993 2550-5876 1220-4545-2106 4435-10976 14301-35698 9645-41574z'
+                          />
+                        </svg>
+               
+                      }
+                      name='AWS'
+                      desc='Experience with the AWS could ecosystem'
+                      rating='4.0'
+                    /> */}
                   </div>
-                  <div className='flex flex-col gap-4 md:w-1/2'>
+                  <div className='flex flex-col gap-4 px-2 md:w-1/2'>
                     <TechnologyCard
                       icon={
                         <svg
